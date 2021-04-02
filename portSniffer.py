ef3765147db39afacb3fb962e34ffcb6701ce4c9
pouriya <pouriya1379@gmail.com>
2021-04-02 23:19:34 +0430

    change checkPort function.

--- portSniffer.py
+++ portSniffer.py
@@ -6,28 +6,36 @@ server=str(input("Enter host name: "))
 portRange=int(input("Enter port number: "))
 timeout=int(input("Enter timeout for scanning: "))
 
-
-def checkPort(port):
+#check port is open or not
+def checkPort(ip,port,timeout):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(timeout)
     try:
-        if (s.connect_ex((server, port))==0):
-            s.close()
+        result=s.connect_ex((ip,port))
+        if result==0:
             return True
         else:
-            s.close()
             return False
-    except Exception as e:
-        print(e)
+
+    except socket.error as e:
+       # print(e)
         return False
 
 
-for port in range(1,portRange):
-    if checkPort(int(port)):
-        print(f"port {port} is open!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
-    else:
-        print(f"port {port} is closed!")
+
+
+
+
+
+
+#
+# for port in range(1,portRange):
+#     if checkPort(server,port,timeout):
+#         print(f"port {port} is open!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+#
+#     else:
+#         print(f"port {port} is closed!")
 
 
 
