4db292c326a5db5aa79e373402f2684dec62cd38
pouriyabp <pouriya1379@gmail.com>
2021-05-01 08:22:47 +0430

    add create_packet function to crate icmp packet but checksum is not work correct.

--- ping.py
+++ ping.py
@@ -1,7 +1,8 @@
 import socket
 import struct
 import random
-
+import os
+import sys
 
 """  
  ICMP Echo Request packets:
@@ -15,6 +16,55 @@ import random
    |                             Payload                           |
    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 """
+ICMP_ECHO_REQUEST = 8
+
+
+def crate_packet(sequence_number=1, packet_size=0):
+    # Maximum for an unsigned short int c object counts to 65535(0xFFFF) we have to sure that our packet id is not
+    # greater than that.
+    identifier = os.getpid() & 0xFFFF
+    # cod is 0 for icmp echo request
+    code = 0
+    # checksum is 0 for now
+    checksum = 0
+    # Header is type (8), code (8), checksum (16), id (16), sequence (16)
+    header = struct.pack('BBHHH', ICMP_ECHO_REQUEST, code, checksum, identifier, sequence_number)
+
+    # Payload Generation
+    payload_byte = []
+    if packet_size > 0:
+        for i in range(0, packet_size):
+            payload_byte += [(i & 0xff)]  # Keep chars in the 0-255 range
+    data = bytes(payload_byte)
+    #checksum = calculate_checksum(header + data)
+    header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, code, checksum, identifier, sequence_number)
+    packet = header + data
+    return packet
+
+
+def calculate_checksum(source_string):
+    # I'm not too confident that this is right but testing seems to
+    # suggest that it gives the same answers as in_cksum in ping.c.
+    sum = 0
+    count_to = (len(source_string) / 2) * 2
+    count = 0
+    while count < count_to:
+        this_val = ord(source_string[count + 1]) * 256 + ord(source_string[count])
+        sum = sum + this_val
+        sum = sum & 0xffffffff  # Necessary?
+        count = count + 2
+    if count_to < len(source_string):
+        print(source_string)
+        sum = sum + ord(source_string[len(source_string) - 1])
+        sum = sum & 0xffffffff  # Necessary?
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum = sum + (sum >> 16)
+    answer = ~sum
+    answer = answer & 0xffff
+    # Swap bytes. Bugger me if I know why.
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    answer = socket.htons(answer)
+    return answer
 
 def change_to_ip(host_name):
     try:
@@ -24,13 +74,8 @@ def change_to_ip(host_name):
         print(e)
         return None
 
+
 if __name__ == "__main__":
     print(change_to_ip('www.google.com'))
     print(socket.getprotobyname('icmp'))
-    # id = int((id(1) * random.random()) % 65535)
-    # print(id)
-    # header = struct.pack('bbHHh', 8, 0, 0, id, 1)
-    # print(header)
-    # data = bytes( 192 * 'Q', 'utf-8')
-    # print(header+data)
-    # print( )
+    print(crate_packet(packet_size=5))
