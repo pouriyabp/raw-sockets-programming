55c5ec172c796bd1ed5dcc1760cda5480715be49
pouriyabp <pouriya1379@gmail.com>
2021-06-06 11:38:48 +0430

    send icmp packet with specific TTL.

--- traceroute.py
+++ traceroute.py
@@ -1,13 +1,20 @@
+import random
 import struct
 import socket
 import sys
 import os
+import time
+
 # ======================================================================================================================
 # ICMP protocol
+
+
 ICMP_ECHO_REQUEST = 8
 
 
-def crate_packet(identifier, sequence_number=1, packet_size=10):  # default packet size is 10 byte.
+# crate ICMP packet with this function
+# default packet size is 60 byte.
+def crate_packet(identifier, sequence_number=1, packet_size=18):  # default packet size is 18 byte.
     # Maximum for an unsigned short int c object counts to 65535(0xFFFF). we have to sure that our packet id is not
     # greater than that.
     identifier = identifier & 0xFFFF
@@ -32,6 +39,7 @@ def crate_packet(identifier, sequence_number=1, packet_size=10):  # default pack
 
 # copy form github https://github.com/Akhavi/pyping/blob/master/pyping/core.py with few changes.
 # The checksum calculation is as follows RFC1071 (https://tools.ietf.org/html/rfc1071)
+# this function is only calculate checksum of packet.
 def calculate_checksum(source_string):
     countTo = (int(len(source_string) / 2)) * 2
     sum = 0
@@ -67,11 +75,23 @@ def calculate_checksum(source_string):
     return answer
 
 
-class IcmpPacket:
-    def __init__(self, packet_size=0):
-        self.type = ICMP_ECHO_REQUEST
-        self.code = 0
-        self.checksum = 0
-        self.id = os.getpid()
-        self.sequence = 0
-        self.packet_size = packet_size
+def send_one_icmp_packet(destination, request_packet, udp_socket):
+    send_time = time.time()
+    try:
+        udp_socket.sendto(request_packet, (destination, 1))
+    except socket.error as e:
+        print(e)
+        return
+    return send_time
+
+
+# test--->
+dst = "8.8.8.8"
+packet_id = os.getpid() + int(random.randint(1, 1000))
+packet = crate_packet(packet_id, packet_size=18)
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.getprotobyname('icmp'))
+udp_socket.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, 4)
+send_one_icmp_packet(dst, packet, udp_socket)
+recvPacket, addr = udp_socket.recvfrom(1024)
+print(recvPacket)
+print(addr)
